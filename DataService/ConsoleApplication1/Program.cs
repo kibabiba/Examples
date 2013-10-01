30e3e895756b753446dd71c6fa010ce362df12fb
kibabiba <kibabiba@mail.ru>
2013-10-01 18:31:32 +0300

    DatServiceAdded

---
+++ DataService/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dt = new TestDataService.Duris4eEntities(new Uri("http://develop.kibabiba.ru/Duris4eDataService.svc"));
+
+            foreach (var row in dt.holidays.Where(p=>p.desc.StartsWith("welcome")))
+            {
+                Console.WriteLine(row.day_time + " " + row.desc);
+            }
+        }
+    }
+}
