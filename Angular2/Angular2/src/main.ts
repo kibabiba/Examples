1eae3b368d051e7588dbbc225d88f32a65921fa1
Kiriil <kirill.kovalev@instafxgroup.com>
2015-09-16 13:44:09 +0200

    Angular2 added

---
+++ Angular2/Angular2/src/main.ts
@@ -0,0 +1,4 @@
+﻿import {bootstrap} from 'angular2/angular2';
+import {MyComponent} from './mycomponent';
+
+bootstrap(MyComponent);
---
+++ Angular2/Angular2/src/mycomponent.ts
@@ -0,0 +1,14 @@
+﻿import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-component'
+})
+@View({
+  template: `<div>Hello, {{message}}</div>`
+})
+export class MyComponent {
+  message : string;
+  constructor() {
+    this.message = 'World';
+  }
+}
