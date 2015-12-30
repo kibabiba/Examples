7e931ac66c3cf90d160cfc78f7e491c1e311802b
Kiriil <kirill.kovalev@instafxgroup.com>
2015-12-30 12:42:50 +0200

    async await

--- AsyncAwait/ConsoleApplication1/Program.cs
+++ AsyncAwait/ConsoleApplication1/Program.cs
@@ -1,44 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Program
     {
         static void Main()
         {
-            Console.WriteLine(DateTime.Now);
-            foreach (var login in GetDataAsync().Result)
-            {
-                Console.WriteLine(login);
-            }
-            Console.WriteLine(DateTime.Now);
+            var time = new Stopwatch();
+            time.Start();
+
+            Console.WriteLine(time.Elapsed);
+            GetDataAsync().Result.ToList().ForEach(Console.WriteLine);
+            Console.WriteLine(time.Elapsed);
             Console.ReadKey();
         }
 
         private static async Task<IEnumerable<int>> GetDataAsync()
         {
-            var p1 = GetAllLogins();
-            var p2 = GetFraudLogin();
+            var p1 = GetAllLoginsAsinc();
+            var p2 = GetFraudLoginAsinc();
 
             return (await p1).Intersect(await p2);
         }
 
-        private static async Task<int[]> GetAllLogins()
+        private static Task<int[]> GetAllLoginsAsinc()
         {
-            return await Task.Run(() =>
+            return Task.Run(() =>
             {
                 Thread.Sleep(5000);
                 return new[] { 1005, 105980 };
             });
         }
 
-        private static async Task<int[]> GetFraudLogin()
+        private static Task<int[]> GetFraudLoginAsinc()
         {
-            return await Task.Run(() =>
+            return Task.Run(() =>
             {
                 Thread.Sleep(5000);
                 return new []{ 105980 };
