453ce2bdb97922b74c45d428909309028be36712
Kirill Kovalev <kibabiba@live.ru>
2022-02-08 22:46:57 +0200

    Visitor with IVisitable

--- Visitor/Visitor/Program.cs
+++ Visitor/Visitor/Program.cs
@@ -7,13 +7,15 @@ namespace Visitor
     {
         static void Main()
         {
-            var kolyan = new Kolyan { Balance = 5000 };
+            IVisitable kolyan = new Kolyan { Balance = 5000 };
             var visitors = new List<IVisitor> {new Police(), new Collectors()};
             foreach (var visitor in visitors)
             {
                 kolyan.Accept(visitor);
             }
             Console.WriteLine("Итого у Коляна осталось {0}", kolyan.Balance);
+
+            Console.ReadKey();
         }
     }
 
@@ -22,12 +24,18 @@ namespace Visitor
         void Visit(Kolyan kolyan);
     }
 
+    public interface IVisitable
+    {
+        int Balance { get; set; }
+        void Accept(IVisitor visitor);
+    }
+
     public class Police : IVisitor
     {
         public void Visit(Kolyan kolyan)
         {
             kolyan.Balance -= 1000;
-            Console.WriteLine("После ГИБДД у Каляна осталось {0} рублей", kolyan.Balance);
+            Console.WriteLine("После ГИБДД у Коляна осталось {0} рублей", kolyan.Balance);
         }
     }
 
@@ -36,14 +44,13 @@ namespace Visitor
         public void Visit(Kolyan kolyan)
         {
             kolyan.Balance -= 2000;
-            Console.WriteLine("После Колекторов у Каляна осталось {0} рублей", kolyan.Balance);
+            Console.WriteLine("После коллекторов у Коляна осталось {0} рублей", kolyan.Balance);
         }
     }
 
-    public class Kolyan
+    public class Kolyan : IVisitable
     {
-        public int Balance;
-
+        public int Balance { get; set; }
         public void Accept(IVisitor visitor)
         {
             visitor.Visit(this);
