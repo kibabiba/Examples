2e40f7285ad0a7fe722fa1d68701e3683d0c7993
Kirill Kovalev <kibabiba@live.ru>
2022-02-09 18:16:33 +0200

    Abstract version of visitor

--- Visitor/Visitor/Program.cs
+++ Visitor/Visitor/Program.cs
@@ -8,7 +8,7 @@ namespace Visitor
         static void Main()
         {
             var kolyan = new Kolyan { Balance = 5000 };
-            var visitors = new List<IVisitor> {new Police(), new Collectors()};
+            var visitors = new List<IVisitor<IPerson>> {new Police(), new Collectors()};
             foreach (var visitor in visitors)
             {
                 kolyan.Accept(visitor);
@@ -19,14 +19,14 @@ namespace Visitor
         }
     }
 
-    public interface IVisitor
+    public interface IVisitor<T>
     {
-        void Visit(IVisitable target);
+        void Visit(IVisitable<T> target);
     }
 
-    public interface IVisitable
+    public interface IVisitable<T>
     {
-        void Accept(IVisitor visitor);
+        void Accept(IVisitor<T> visitor);
     }
 
     public interface IPerson
@@ -34,28 +34,28 @@ namespace Visitor
         int Balance { get; set; }
     }
 
-    public class Police : IVisitor
+    public class Police : IVisitor<IPerson>
     {
-        public void Visit(IVisitable target)
+        public void Visit(IVisitable<IPerson> target)
         {
             var balance = ((IPerson)target).Balance -= 2000;
             Console.WriteLine("После ГИБДД у Коляна осталось {0} рублей", balance);
         }
     }
 
-    public class Collectors : IVisitor
+    public class Collectors : IVisitor<IPerson>
     {
-        public void Visit(IVisitable target)
+        public void Visit(IVisitable<IPerson> target)
         {
             var balance = ((IPerson)target).Balance -= 1000;
             Console.WriteLine("После коллекторов у Коляна осталось {0} рублей", balance);
         }
     }
 
-    public class Kolyan : IVisitable, IPerson
+    public class Kolyan : IVisitable<IPerson>, IPerson
     {
         public int Balance { get; set; }
-        public void Accept(IVisitor visitor)
+        public void Accept(IVisitor<IPerson> visitor)
         {
             visitor.Visit(this);
         }
